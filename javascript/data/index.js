02bfa0eb66f067ee6037d671ea13caff3f7541cc
Ki Jung Kim <kimkijung@icloud.com>
2024-09-24 22:49:12 -1000

    Add data and logic to generate window options

--- javascript/data/index.js
+++ javascript/data/index.js
@@ -1,30 +1,84 @@
 export function data() {
+  const windowOptions = {
+    edit: 'Edit',
+    favorites: 'Favorites',
+    file: 'File',
+    go: 'Go',
+    help: 'Help',
+    search: 'Search',
+    tools: 'Tools',
+    view: 'View'
+  }
+
+  const windowType = {
+    notepad: 'notepad',
+    terminal: 'terminal',
+    folder: 'folder'
+  }
+
   const programs = [
     {
       name: 'My Computer',
       icon: 'assets/computer.png',
+      options: [
+        windowOptions.file,
+        windowOptions.edit,
+        windowOptions.view,
+        windowOptions.tools,
+        windowOptions.help
+      ]
     },
     {
       name: 'About Me',
       icon: 'assets/help-book.png',
-      class: '--line-below'
+      class: '--line-below',
+      options: [
+        windowOptions.file,
+        windowOptions.view,
+        windowOptions.help
+      ]
+    },
+    {
+      name: 'Stack',
+      icon: 'assets/console-prompt.png',
+      options: [
+        windowOptions.file,
+        windowOptions.edit,
+        windowOptions.view,
+        windowOptions.help
+      ]
     },
     {
       name: 'Experience',
-      icon: 'assets/console-prompt.png'
-    },
-    {
-      name: 'Skills',
-      icon: 'assets/hard-disk.png'
+      icon: 'assets/hard-disk.png',
+      options: [
+        windowOptions.file,
+        windowOptions.edit,
+        windowOptions.view,
+        windowOptions.tools,
+        windowOptions.help
+      ]
     },
     {
       name: 'Education',
       icon: 'assets/notepad.png',
-      class: '--line-below'
+      class: '--line-below',
+      options: [
+        windowOptions.file,
+        windowOptions.edit,
+        windowOptions.search,
+        windowOptions.help
+      ]
     },
     {
-      name: 'Contact Me',
-      icon: 'assets/outlook.png'
+      name: 'Contact Info',
+      icon: 'assets/outlook.png',
+      options: [
+        windowOptions.file,
+        windowOptions.edit,
+        windowOptions.view,
+        windowOptions.help
+      ]
     },
   ];
   return {
